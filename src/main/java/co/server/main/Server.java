080352975d2faa50b6cce53fc6c0275d8238a384
LuisJava10 <2220171020@estudiantesunibauge.edu.co>
2021-04-10 16:49:22 -0500

    Se corrigieron errores del servidor

--- src/main/java/co/server/main/Server.java
+++ src/main/java/co/server/main/Server.java
@@ -5,14 +5,28 @@
  */
 package co.server.main;
 
+import dto.DTOArticulo;
+import dto.DTOServicioTecnico;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.net.ServerSocket;
+import java.net.Socket;
+import modelos.ListaDoblementeEnlazadaGenerica;
+import utils.ComandosSql;
 
 /**
  *
  * @author Samuel
  */
 public class Server {
-        ServerSocket socketServer = new ServerSocket(7070);
+        
+     public static void main(String[] args) {
+        //ServerSocket socketServer = null;
+
+        while (true) {
+            try {
+                ServerSocket socketServer = new ServerSocket(7070);
                 System.out.println("El servidor está escuchando...");
                 Socket socket = socketServer.accept();
 
@@ -32,7 +46,78 @@ public class Server {
                     servicio = (DTOServicioTecnico) object;
                     comando = servicio.getComando();
                 }
-socketServer.close();
+                //System.out.println("SVR: " + comando);
+                switch (comando) {
+
+                    case ComandosSql.ARTICULO_CREAR:
+                        new DAOArticulo().insertar(articulo);
+                        objectOutputStream.writeObject(new ListaDoblementeEnlazadaGenerica());
+                        break;
+
+                    case ComandosSql.ARTICULO_CONSULTAR_POR_ID:
+                        object = new DAOArticulo().consultar(articulo.getId());
+                        objectOutputStream.writeObject(object); 
+                        break;
+
+                    case ComandosSql.ARTICULO_CONSULTAR_TODOS:
+                        
+                        
+                        object = new DAOArticulo().consultarTodos();
+                        
+                        objectOutputStream.writeObject(object);
+                        break;
+                    
+                    case ComandosSql.ARTICULO_ACTUALIZAR_POR_ID:
+                        new DAOArticulo().actualizar(articulo);
+                        objectOutputStream.writeObject(new ListaDoblementeEnlazadaGenerica());
+                        break;
+                    
+                    case ComandosSql.ARTICULO_ELIMINAR_POR_ID:
+                        new DAOArticulo().eliminar(articulo.getId());
+                        objectOutputStream.writeObject(new ListaDoblementeEnlazadaGenerica());
+                        break;
+                    //Aqui
+                    case ComandosSql.CATEGORIA_CONSULTAR_TODAS:
+                        object = new DAOArticulo().consultarCategorias();
+                        
+                        objectOutputStream.writeObject(object);
+                        break;
+                        
+                   //Inicio construccion switch servicios.
+                     case ComandosSql.SERVICIO_CREAR:
+                        new DAOTecnico().insertar(servicio);
+                        objectOutputStream.writeObject(new ListaDoblementeEnlazadaGenerica());
+                        break;
+
+                    case ComandosSql.SERVICIO_CONSULTAR_POR_ID:
+                        object = new DAOTecnico().consultar(articulo.getId());
+                        
+                        objectOutputStream.writeObject(object);
+
+                        break;
+
+                    case ComandosSql.SERVICIO_CONSULTAR_TODOS:
+                        //AQUI
+                        
+                        object = new DAOTecnico().consultarTodos();
+                        
+                        objectOutputStream.writeObject(object);
+                        break;
+                        
+                    
+                    case ComandosSql.SERVICIO_ACTUALIZAR_POR_ID:
+                        new DAOTecnico().actualizar(articulo);
+                        objectOutputStream.writeObject(new ListaDoblementeEnlazadaGenerica());
+                        break;
+                    
+                    case ComandosSql.SERVICIO_ELIMINAR_POR_ID:
+                        new DAOArticulo().eliminar(articulo.getId());
+                        objectOutputStream.writeObject(new ListaDoblementeEnlazadaGenerica());
+                        break;
+ 
+                }
+
+                socketServer.close();
             } catch (ClassNotFoundException | IOException e) {
                 e.printStackTrace();
             }
